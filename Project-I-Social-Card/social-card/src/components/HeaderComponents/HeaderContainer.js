d0e20b66a1c5d156e3a1ad9b6978c309d0076728
Leslie Thompson <lcrthompson@gmail.com>
2019-04-01 14:27:16 -0700

    initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderContent from "./components/HeaderComponents/HeaderContent.js";
+// import "./components/HeaderComponents/HeaderTitle.js";
+// import "./components/HeaderComponents/ImageThumbnail.js";
+
+const HeaderContainer = () => {
+  return (
+    <div className="header">
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderContainer;
