e57da3c40f101701ac69bc98bb4eb335ac12cad5
Leslie Thompson <lcrthompson@gmail.com>
2019-04-01 15:43:21 -0700

    header refined

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HeaderTitle = () => {
-  return <h1>Lambda School @LambdaSchool 26 jan</h1>;
+  return (
+    <div className="title">
+      <h1>Lambda School</h1>{" "}
+      <p className="smalltitle">@LambdaSchool - 26 jan</p>
+    </div>
+  );
 };
 
 export default HeaderTitle;
