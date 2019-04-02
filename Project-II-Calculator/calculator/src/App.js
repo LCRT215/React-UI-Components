4e7185ca919a8e09cdbabbe04b9b93daff346427
Leslie Thompson <lcrthompson@gmail.com>
2019-04-02 16:40:27 -0700

    revamped and finally starting to get it!

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,7 +7,34 @@ const App = () => {
   return (
     <div className="container">
       <CalculatorDisplay />
-      <NumberButton />
+      <div className="number-containers">
+        <div className="row1">
+          <NumberButton buttonStyle="clear" text="clear" />
+          <NumberButton buttonStyle="operations top" text="÷" />
+        </div>
+        <div className="row1">
+          <NumberButton buttonStyle="number" text="7" />
+          <NumberButton buttonStyle="number" text="8" />
+          <NumberButton buttonStyle="number" text="9" />
+          <NumberButton buttonStyle="operations" text="x" />
+        </div>
+        <div className="row2">
+          <NumberButton buttonStyle="number" text="4" />
+          <NumberButton buttonStyle="number" text="5" />
+          <NumberButton buttonStyle="number" text="6" />
+          <NumberButton buttonStyle="operations" text="-" />
+        </div>
+        <div className="row3">
+          <NumberButton buttonStyle="number" text="1" />
+          <NumberButton buttonStyle="number" text="2" />
+          <NumberButton buttonStyle="number" text="3" />
+          <NumberButton buttonStyle="operations" text="+" />
+        </div>
+        <div className="row4">
+          <NumberButton buttonStyle="zero" text="0" />
+          <NumberButton buttonStyle="operations top" text="=" />
+        </div>
+      </div>
       {/* <FooterContainer /> */}
     </div>
   );
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,40 +1,41 @@
 import React from "react";
 import "./Button.css";
 
-const NumberButton = () => {
+const NumberButton = props => {
   return (
-    <div className="number-containers">
-      <div className="row1">
-        <button className="clear">clear</button>
-        <button className="operations top">÷</button>
-      </div>
+    <button className={props.buttonStyle}>{props.text}</button>
+    // <div className="number-containers">
+    //   <div className="row1">
+    //     <button className="clear">clear</button>
+    //     <button className="operations top">÷</button>
+    //   </div>
 
-      <div className="row1">
-        <button className="number">7</button>
-        <button className="number">8</button>
-        <button className="number">9</button>
-        <button className="operations">x</button>
-      </div>
+    //   <div className="row1">
+    //     <button className="number">7</button>
+    //     <button className="number">8</button>
+    //     <button className="number">9</button>
+    //     <button className="operations">x</button>
+    //   </div>
 
-      <div className="row2">
-        <button className="number">4</button>
-        <button className="number">5</button>
-        <button className="number"> 6</button>
-        <button className="operations">-</button>
-      </div>
+    //   <div className="row2">
+    //     <button className="number">4</button>
+    //     <button className="number">5</button>
+    //     <button className="number"> 6</button>
+    //     <button className="operations">-</button>
+    //   </div>
 
-      <div className="row3">
-        <button className="number">1</button>
-        <button className="number">2</button>
-        <button className="number">3</button>
-        <button className="operations">+</button>
-      </div>
+    //   <div className="row3">
+    //     <button className="number">1</button>
+    //     <button className="number">2</button>
+    //     <button className="number">3</button>
+    //     <button className="operations">+</button>
+    //   </div>
 
-      <div className="row4">
-        <button className="zero">0</button>
-        <button className="operations top">=</button>
-      </div>
-    </div>
+    //   <div className="row4">
+    //     <button className="zero">0</button>
+    //     <button className="operations top">=</button>
+    //   </div>
+    // </div>
   );
 };
 
